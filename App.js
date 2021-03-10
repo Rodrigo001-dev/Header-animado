f5c31dedee30fa51577783eff534267b60a95f3d
Rodrigo Rael <rodrigorael53@gmail.com>
2021-03-10 16:35:31 -0300

    add config

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { 
-  View, 
-  SafeAreaView, 
-  StyleSheet, 
-  Image, 
-  ScrollView, 
-  Animated 
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Animated,
 } from 'react-native';
 
 export default function App() {
@@ -13,26 +13,28 @@ export default function App() {
 
   return (
     <SafeAreaView>
-      <Animated.View style={[
-        styles.header,
-        {
-          height: scrollY.interpolate({
-            inputRange: [10, 160, 185],
-            outputRange: [140, 20, 0],
-            // extrapolate: 'clamp' quer dizer que quando chegar no 0 que é o
-            // final não vai fazer mais nada, ou seja, não vai volar para a
-            // posição inicial, quando chegar no 0(na opacidade 0) ele vai ficar
-            // nessa opacidade 0
-            extrapolate: 'clamp'
-          }),
-          opacity: scrollY.interpolate({
-            inputRange: [1, 75, 170],
-            outputRange: [1, 1, 0],
-            extrapolate: 'clamp'
-          }),
-        }
-      ]}>
-        <Image 
+      <Animated.View
+        style={[
+          styles.header,
+          {
+            height: scrollY.interpolate({
+              inputRange: [10, 160, 185],
+              outputRange: [140, 20, 0],
+              // extrapolate: 'clamp' quer dizer que quando chegar no 0 que é o
+              // final não vai fazer mais nada, ou seja, não vai volar para a
+              // posição inicial, quando chegar no 0(na opacidade 0) ele vai ficar
+              // nessa opacidade 0
+              extrapolate: 'clamp',
+            }),
+            opacity: scrollY.interpolate({
+              inputRange: [1, 75, 170],
+              outputRange: [1, 1, 0],
+              extrapolate: 'clamp',
+            }),
+          },
+        ]}
+      >
+        <Image
           source={require('./src/assets/cam.png')}
           style={{ width: 30, height: 30 }}
           // resizeMode="contain" vai exibir todo o tamanho da imagem
@@ -45,14 +47,14 @@ export default function App() {
             width: scrollY.interpolate({
               inputRange: [0, 120],
               outputRange: [200, 120],
-              extrapolate: 'clamp'
+              extrapolate: 'clamp',
             }),
-            height: 90
+            height: 90,
           }}
           resizeMode="contain"
         />
 
-        <Image 
+        <Image
           source={require('./src/assets/send.png')}
           style={{ width: 30, height: 30 }}
           resizeMode="contain"
@@ -63,12 +65,16 @@ export default function App() {
         // o scrollEventThrottle vai controlar quantos px que vai mandando quando
         // está dando o scroll
         scrollEventThrottle={16}
-        onScroll={Animated.event([{
-          nativeEvent: {
-            contentOffset: { y: scrollY }
-          },
-        }],
-        { useNativeDriver: false })}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: { y: scrollY },
+              },
+            },
+          ],
+          { useNativeDriver: false },
+        )}
       >
         <View style={styles.box}></View>
         <View style={styles.box}></View>
@@ -80,7 +86,7 @@ export default function App() {
       </ScrollView>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   header: {
